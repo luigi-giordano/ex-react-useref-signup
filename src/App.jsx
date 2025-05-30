d7ca10858918e5d8280d217a73b16ef2c6b6a781
luigi-giordano <luigi.giordano1992@gmail.com>
2025-05-30 12:32:52 +0200

    [creation input+value]

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,74 @@
-import React from 'react'
+import { useState } from "react"
 
 function App() {
+
+  const [fullName, setFullName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [specialization, setSpecialization] = useState("");
+  const [experienceYears, setExperienceYears] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  }
+
+
   return (
-    <div>App</div>
+    <div>
+      <h1>Web Developer Signup</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>Nome Completo</p>
+          <input
+            type="text"
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
+          />
+        </label>
+        <label>
+          <p>Username</p>
+          <input type="text"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        </label>
+        <label>
+          <p>Password</p>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <label>
+          <p>Specializzazione</p>
+          <select
+            value={specialization}
+            onChange={(e) => setSpecialization(e.target.value)}
+          >
+            <option value="Full Stack">Full Stack</option>
+            <option value="Frontend">Frontend</option>
+            <option value="Backend">Backend</option>
+          </select>
+        </label>
+        <label>
+          <p>Anni di esperienza</p>
+          <input
+            type="number"
+            value={experienceYears}
+            onChange={(e) => setExperienceYears(e.target.value)}
+          />
+        </label>
+        <label>
+          <p>Descrizione</p>
+          <textarea
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </label>
+      </form>
+    </div>
   )
 }
 
